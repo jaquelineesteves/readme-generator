871ce29022f1ec256c37df75aa8bd1c2105d89a8
jaquelineesteves <jaaqfariasz@gmail.com>
2024-05-02 21:52:55 -0400

    finished

--- README.md
+++ README.md
@@ -1,15 +1,14 @@
 
-  # readme Generator
-  ![MIT](undefined)
+  # Readme Generator
+  ![MIT](https://img.shields.io/static/v1?label=License&message=MIT&color=blue)
   
   ## Description 
-   build to create readme by using node in comman line
+   Build to create a readme file by using node in command-line, using less time writing it.
 
   ## Table of Contents 
   * [Installation](#installation)
   * [Usage](#usage)
   * [License](#license)
-  * [Contributing](#contributing)
   * [Tests](#tests)
   * [Questions](#questions)
   
@@ -26,10 +25,8 @@
 
 
   ## License 
-  MIT  
+  This project is licensed under MIT license  
 
-  ## Contributing  
-  undefined
 
   ## Tests
   To run tests, run the following command:
@@ -40,3 +37,7 @@ npm test
 
   ## Questions
   If you have any questions about the repo, open an issue or contact me directly at jaaqfariasz@gmail.com. You can find more of my work at [jaquelineesteves](https://github.com/jaquelineesteves/).
+
+  ## Video
+  https://drive.google.com/file/d/18DuB4kaj7uLVsG6DUy05UQjhmzcU_OsL/view?usp=sharing
+
--- generateMarkdown.js
+++ generateMarkdown.js
@@ -1,30 +1,30 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
-// If there is no license, return an empty string
-function renderLicenseBadge(license) {
-}
-
-console.log(renderLicenseBadge('MIT'));
-// TODO: Create a function that returns the license link
-// If there is no license, return an empty string
-
 
 function renderLicenseLink(license) {
+if(license !== 'None'){
+  let licenseChosen = license
+  let licensenospace = licenseChosen
+  .split(" ")
+  .join("");
 
-// 'MIT'  badge = 'https://img.shields.io/badge/License-MIT-yellow.svg'
-// 'APACHE 2.0'  badge = 'https://img.shields.io/badge/License-Apache_2.0-blue.svg'
-// 'GPL 3.0' badge = 'https://img.shields.io/badge/License-GPLv3-blue.svg'
-// 'BSD 3'  badge = 'https://img.shields.io/badge/License-BSD_3--Clause-blue.svg'
+  return `![${license}](https://img.shields.io/static/v1?label=License&message=${licensenospace}&color=blue)`
 }
-// TODO: Create a function that returns the license section of README
-// If there is no license, return an empty string
+ else return ''
 
-function renderLicenseSection(license) {}
+}
+
+function renderLicenseSection(license) {
+  if(license !== 'None'){
+
+    return `This project is licensed under ${license} license`
+  }
+   else return ''
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `
   # ${data.title}
-  ![${data.license}](${renderLicenseBadge(data.license)})
+  ${renderLicenseLink(data.license)}
   
   ## Description 
    ${data.description}
@@ -33,7 +33,6 @@ function generateMarkdown(data) {
   * [Installation](#installation)
   * [Usage](#usage)
   * [License](#license)
-  * [Contributing](#contributing)
   * [Tests](#tests)
   * [Questions](#questions)
   
@@ -50,10 +49,8 @@ function generateMarkdown(data) {
 
 
   ## License 
-  ${data.license}  
+  ${renderLicenseSection(data.license)}  
 
-  ## Contributing  
-  ${data.contributing}
 
   ## Tests
   To run tests, run the following command:
